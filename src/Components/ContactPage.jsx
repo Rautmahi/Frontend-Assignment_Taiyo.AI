70048b929bb75a6a8ca72f2ff6c04c24bbeca0fb
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2023-04-13 15:10:56 +0530

    navbar and sidebar created

---
+++ src/Components/ContactPage.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const ContactPage = () => {
+    const [showModal, setShowModal] = React.useState(false);
+  return (
+    <div>
+        <div style={{"width":"100%","height":"50px","marginTop":"-773px","backgroundColor":"teal"}}>
+        <h2 style={{"color":"white"}}>Contact Page</h2>
+    </div>
+
+{/* form data */}
+    
+    
+      <button
+        className="bg-red-600 text-black active:bg-blue-500"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+       Create Contact
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+               
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    
+  
+
+
+    </div>
+  )
+}
+
+export default ContactPage
